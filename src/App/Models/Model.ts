875aa8e13e47afd42411a454853fd426419e6587
Monzurul Hasan <monzurulhasan1001@gmail.com>
2024-07-08 20:27:50 +0600

    Update Model methods

--- src/App/Models/Model.ts
+++ src/App/Models/Model.ts
@@ -22,7 +22,7 @@ export class Model {
         return this.table;
     }
 
-    public static async insert<Payload, Result>(
+    public static async create<Payload, Result>(
         data: Payload
     ): ResponseType<Result> {
         const [result] = await this.table.insert(data).returning("*");
